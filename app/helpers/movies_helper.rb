694249da5bd1b8a1eef8822182ac062cc9d23895
EC2 Default User <ec2-user@ip-172-31-6-249.us-east-2.compute.internal>
2019-02-13 01:38:18 +0000

    part 1 complete

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,4 +3,11 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
+  def sort_color(header)
+    if(params[:sort].to_s == header)
+      return 'hilite'
+    else
+      return nil
+    end
+  end
 end
