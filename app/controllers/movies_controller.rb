478c058fae4e236db269a2d3f2332a2c7a537270
EC2 Default User <ec2-user@ip-172-31-6-249.us-east-2.compute.internal>
2019-02-13 05:18:58 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,31 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.order(params[:sort])
+    all_rating_keys = Movie.rating_values #get all the ratings that are in the db
+    if all_rating_keys #check for nil
+      @all_ratings = Hash.new #create the new hash for what will be displayed on the view
+      all_rating_keys.each do |rate| 
+        @all_ratings[rate] = true #create a tuple of ratings, checked value for display
+#what this part of the code does is, if the view is sending back checkbox params
+#then set their corresponding checked values as true for display
+#otherwise set them as false
+        if params[:ratings]
+          if params[:ratings][rate] == nil
+            @all_ratings[rate] = false
+          end
+        end
+      end
+    end
+#if the sort params are coming in, then preserve the previous excercise's efforts    
+    if params[:sort]
+      @movies = Movie.order(params[:sort])
+    elsif params[:ratings] #if we are getting ratings as params, filter the results
+      ratings_list = params[:ratings].keys
+      @movies = Movie.with_ratings(ratings_list)
+    else
+      @movies = Movie.all
+    end
+
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ActiveRecord::Base
+    def self.rating_values #gets all available rating values from db
+        Movie.uniq.pluck(:rating)
+    end
+    
+    def self.with_ratings(ratings_list) #filters movies according to ratings
+        Movie.where(:rating => ratings_list).all
+    end
 end
